327736c95ca8aac63590d13b179fa4e518cfd39c
alex-mint <holzeimer@mail.de>
2020-03-01 14:37:03 +0100

    add params to script

--- docs/scripts/plc_scl_make_fct.py
+++ docs/scripts/plc_scl_make_fct.py
@@ -19,25 +19,33 @@ signal_changed=>"MI_diningWest_leftDown");
 
 """
 
-def get_names(fname='transfer.txt'):
+import argparse
+
+def get_names(fname):
     with open(fname) as f:
         content = f.readlines()
         content = [x.strip() for x in content]
         print(f'content from list {content}')
         return content
 
-def make_file(list_fct, new_file='scl_output.txt'):
-    file = open(new_file, "w")
+def make_file(output_file, list_fct):
+    file = open(output_file, "w")
     
     for item in list_fct:
         file.write(f'#{item}(signal_input:="I{item}",\n') 
         file.write(f'input_name:=\'{item}\',\n')
         file.write(f'signal_changed=>"MI_{item}");\n\n')
     file.close()
+    print(f'writing file {output_file} -> done')
 
 def main():
-    list_fct = get_names()
-    make_file(list_fct)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-i", "--input_file", default='input_file.txt')
+    parser.add_argument("-o", "--output_file", default='output_file.txt')
+    args = parser.parse_args()
+    
+    list_fct = get_names(args.input_file)
+    make_file(args.output_file, list_fct)
 
 if __name__ == "__main__":
     main()
